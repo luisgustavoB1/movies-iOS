9758e22c006922fd6e51ef4dfca260b4235cb191
Luis Gustavo <luisgustavob1@outlook.com>
2024-05-12 10:40:54 +0200

    Feature: Project films

---
+++ Movies/Resources/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Luis Gustavo on 10/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
